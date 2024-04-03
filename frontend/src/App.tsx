98c1cab2495a5ff7e7b80ff19803f7762e390ac9
edymusajev <edymusajev@gmail.com>
2024-04-03 12:58:48 +0700

    x

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
 function App() {
-  return <>hello</>;
+  return (
+    <div>
+      <div className="h-10 border-b flex items-center px-2">
+        <span className="text-muted text-sm font-bold ">PROJECT MANAGER</span>
+      </div>
+      <div className="grid grid-cols-12 divide-x h-[calc(100vh-40px)]">
+        <div className="col-span-3">
+          <p>Projects</p>
+        </div>
+        <div>
+          <p>Details</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
